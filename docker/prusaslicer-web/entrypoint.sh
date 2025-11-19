3a60b59141a7c12316e591676ce24b0990c5623f
Julianne Swinoga <julianneswinoga@gmail.com>
2025-11-19 17:46:05 -0500

    Initial web working

---
+++ docker/prusaslicer-web/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -eu
+
+export VNC_PORT=${VNC_PORT:-5900}
+
+# turbovnc options
+export DISPLAY=${DISPLAY:-:0}
+export VNC_RESOLUTION=${VNC_RESOLUTION:-1280x800}
+export VNC_SEC='-securitytypes TLSNone,X509None,None'
+
+
+DISPLAY_NUMBER=$(echo $DISPLAY | cut -d: -f2)
+
+# novnc options
+export NOVNC_PORT="${NOVNC_PORT:-8080}"
+export LOCALFBPORT=$((${VNC_PORT} + DISPLAY_NUMBER))
+
+# prusaslicer options
+export VGL_DISPLAY="${VGL_DISPLAY:-egl}"
+export VGLRUN=
+
+export SUPD_LOGLEVEL="${SUPD_LOGLEVEL:-TRACE}"
+exec gosu slic3r supervisord -e "$SUPD_LOGLEVEL"
